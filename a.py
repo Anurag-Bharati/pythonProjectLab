b2ab217637f7c344e8fa947faba4ac91d7e3c2de
Anurag-Bharati <anuragbharati26@gmail.com>
2021-02-10 12:23:21 +0545

    none

---
+++ a.py
@@ -0,0 +1,7 @@
+# program to calculate the Perimeter of a Rect.
+
+length = float(input("Enter a value for Length : "))
+breadth = float(input("Enter a value for Breadth : "))
+Perimeter = 2*(length+breadth)
+print(f"The Perimeter of rectangle is (2*({length}+{breadth}) = {Perimeter} ")
+print(type(Perimeter))
